9aa3cda81d3a0c7ea1cdd07bee283f537ce03d20
Sukanya <sukanya@rippling.com>
2021-03-25 16:25:03 +0530

    Ignore docker tests.

--- rack/spec/docker/backend_docker_spec.rb
+++ rack/spec/docker/backend_docker_spec.rb
@@ -3,8 +3,8 @@ require "docker"
 
 describe "Orders Dockerfile" do
   before(:all) do
-    @image = Docker::Image.build_from_dir('./../containers/backend/orders')
-    @image.tag(repo: 'test/orders', tag: 'test')
+#     @image = Docker::Image.build_from_dir('./../containers/backend/orders')
+#     @image.tag(repo: 'test/orders', tag: 'test')
 
     set :os, family: :alpine
     set :backend, :docker
@@ -22,8 +22,8 @@ end
 
 describe "Payment Dockerfile" do
   before(:all) do
-    @image = Docker::Image.build_from_dir('./../containers/backend/payment')
-    @image.tag(repo: 'test/payment', tag: 'test')
+#     @image = Docker::Image.build_from_dir('./../containers/backend/payment')
+#     @image.tag(repo: 'test/payment', tag: 'test')
 
     set :os, family: :alpine
     set :backend, :docker
